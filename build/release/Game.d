9a941f4e4732effba3d076a4da55fecf3eb79794
Justin Walter <waltjust@eos24.cis.gvsu.edu>
2021-02-05 12:55:10 -0500

    removed bin/build directories

--- build/release/Game.d
+++
@@ -1,3 +0,0 @@
-build/release/Game.o: src/Game.cpp include/Game.hpp
-
-include/Game.hpp:
--- build/release/main.d
+++
@@ -1,5 +0,0 @@
-build/release/main.o: src/main.cpp include/catch.hpp include/Game.hpp
-
-include/catch.hpp:
-
-include/Game.hpp:
